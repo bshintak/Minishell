6ac08be0bfdaf363902f2a58f33d7e3d60d991dd
Beatriz Schileo Shintaku <87424758+bshintak@users.noreply.github.com>
2022-07-27 16:15:00 +0100

    Add files via upload

--- src/echo.c
+++ src/echo.c
@@ -6,7 +6,7 @@
 /*   By: bshintak <bshintak@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/27 11:35:18 by bshintak          #+#    #+#             */
-/*   Updated: 2022/07/27 11:47:39 by bshintak         ###   ########.fr       */
+/*   Updated: 2022/07/27 12:14:34 by bshintak         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,7 +20,19 @@ void	builtin_echo(char *line)
 	if (line[i] == 'e' && line[i + 1] == 'c' 
 		&& line[i + 2] == 'h' && line[i + 3] == 'o')
 		i += 5;
+	else if (is_space(line [i + 4] == 0) && line[i + 4] != '\0')
+	{
+		printf("command not found\n");
+		return ;
+	}
+	if (line[5] == '-' && line[6] == 'n')
+		i += 3;
 	while (line[i])
+	{
+		if (line[i] == '"')
+			i++;
 		printf("%c", line[i++]);
-	printf("\n");
+	}
+	if (!(line[5] == '-' && line[6] == 'n'))
+		printf("\n");
 }
