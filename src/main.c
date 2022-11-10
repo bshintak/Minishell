6b0a408babf3be1c78547c3ba06c5a9dd3de8dde
Beatriz schileo Shintaku <bshintak@c1r20s5.42lisboa.com>
2022-11-10 12:41:29 +0000

    struct

--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@
 /*   By: bshintak <bshintak@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/21 16:58:37 by bshintak          #+#    #+#             */
-/*   Updated: 2022/11/03 15:25:52 by bshintak         ###   ########.fr       */
+/*   Updated: 2022/11/10 12:30:42 by bshintak         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,6 +16,11 @@ int main(int argc, char **argv)
 {
 	char 		*line;
 
+	if (argc != 1 && argv[1])
+	{
+		printf("wrong argument!\n");
+		return (-1);
+	}
 	while (1)
 	{
 		line = readline("➜  MiniShell: ");
@@ -24,4 +29,5 @@ int main(int argc, char **argv)
 		add_history(line);
 		parser(line);
 	}
+	return (0);
 }
--- src/parser.c
+++ src/parser.c
@@ -6,16 +6,16 @@
 /*   By: bshintak <bshintak@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/26 13:11:37 by bshintak          #+#    #+#             */
-/*   Updated: 2022/11/07 11:22:18 by bshintak         ###   ########.fr       */
+/*   Updated: 2022/11/10 12:01:27 by bshintak         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../minishell.h"
 
-int	check_command(char *token)
-{
-	return (access(token, F_OK | X_OK));
-}
+// int	check_command(char *token)
+// {
+// 	return (access(token, F_OK | X_OK));
+// }
 
 void	parser(char *line)
 {
@@ -34,8 +34,8 @@ void	parser(char *line)
 		printf("token = %s\n", token);
 		id = get_id(token);
 		printf("id = %d\n", id);
-		if (id == ID_WORD)
-			printf("access = %d\n", check_command(token));
+		// if (id == ID_WORD)
+		// 	printf("access = %d\n", check_command(token));
 		reset = 0;
 	}
 }
